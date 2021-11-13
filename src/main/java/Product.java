6235e630ab6787a3e48a80acd673459f88208c9a
NoahSeligson <noahseligson18@gmail.com>
2021-11-13 13:49:31 -0500

    Added Method to get Website Link

--- src/main/java/Product.java
+++ src/main/java/Product.java
@@ -1,2 +1,27 @@
 public class Product {
+    private String name;
+    private double value;
+    private String websiteLink;
+
+    Product(String name, double value, String websiteLink)
+    {
+        this.name = name;
+        this.value = value;
+        this.websiteLink = websiteLink;
+    }
+
+    private String getName()
+    {
+        return this.name;
+    }
+
+    private double getValue()
+    {
+        return this.value;
+    }
+
+    private String getWebsiteLink()
+    {
+        return this.websiteLink;
+    }
 }
--- src/main/java/Website.java
+++ src/main/java/Website.java
@@ -1,2 +1,20 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 public class Website {
+
+    public String getLink(String name, String console)
+    {
+        try{
+            Document doc = Jsoup.connect("https://www.google.com.au/search?q=gamestop" + name + console).get();
+            Elements links = doc.getElementsByClass("g");
+            Element firstLink = links.first();
+            return firstLink.getElementsByTag("a").first().attr("href");
+        }catch(Exception ex){
+            System.out.println("An error has occurred :(");
+        }
+        return null;
+    }
 }
