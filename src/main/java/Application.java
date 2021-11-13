2921a9aa2ff1bee8d6f7147b06ce35efe3c859cb
NoahSeligson <noahseligson18@gmail.com>
2021-11-13 15:09:03 -0500

    Completed Website class

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -4,6 +4,7 @@
     GameStop Inventory Manager
  */
 
+
 public class Application {
     public static void main (String[] args)
     {
--- src/main/java/Website.java
+++ src/main/java/Website.java
@@ -3,6 +3,14 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Scanner;
+
+import java.util.*;
+
 public class Website {
 
     public String getLink(String name, String console)
@@ -16,5 +24,33 @@ public class Website {
             System.out.println("An error has occurred :(");
         }
         return null;
+    }
+
+    public double getPrice(URL url)
+    {
+        try {
+            System.out.println(url.toString());
+            URLConnection connection = url.openConnection();
+            InputStream is = connection.getInputStream();
+            Scanner scan = new Scanner(new InputStreamReader(is));
+            String string = "";
+
+            for(int i = 0; i < 5; i++)
+            {
+                while (!(string.contains("<span class=\"actual-price \">"))) {
+                    string = scan.nextLine();
+                }
+
+                while(!string.contains("$"))
+                {
+                    string = scan.nextLine();
+
+                }
+            }
+            return Double.parseDouble(string.trim().substring(1));
+        }catch(java.io.IOException ex){
+            System.out.println("\nAn error has occurred. :( \n");
+        }
+        return -1;
     }
 }
